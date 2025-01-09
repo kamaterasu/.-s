acef956a245b681b89383658449c0775234b29a1
kamaterasu <togoldor.bayrtsolmon@gmail.com>
2025-01-09 09:19:24 +0800

    Add configuration files and initial setup for various applications

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,10 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local keymap = vim.api.nvim_set_keymap
+
+local opts = { noremap = true, silent = true }
+keymap("n", "<Up>", "<Nop>", opts)
+keymap("n", "<Down>", "<Nop>", opts)
+keymap("n", "<Left>", "<Nop>", opts)
+keymap("n", "<Right>", "<Nop>", opts)
